f19fd1e0769af5dec3e54336f43a712b944378f0
Nicolò Scialpi <nicoloscialpi@gmail.com>
2022-01-26 23:10:23 +0100

    Removed password until i do encryption

--- popup.js
+++ popup.js
@@ -2,16 +2,13 @@ function readyFuntion() {
 
     console.log("Ready");
     let inputUser = $("#input_username");
-    let inputPass = $("#input_pass");
     let inputTypeStudenti = $("#default_radio_studenti");
     let inputTypeProf = $("#default_radio_prof");
 
     chrome.storage.sync.get("defaultUser", ({ defaultUser }) => {
-        inputUser.val(defaultUser);
-    });
-
-    chrome.storage.sync.get("defaultPass", ({ defaultPass }) => {
-        inputPass.val(defaultPass);
+        if (defaultUser) {
+            inputUser.val(defaultUser);
+        }
     });
 
     chrome.storage.sync.get("defaultType", ({ defaultType }) => {
@@ -28,10 +25,6 @@ function readyFuntion() {
         let defaultUser = inputUser.val();
         chrome.storage.sync.set({ defaultUser });
         console.log("Changed username to " + defaultUser);
-    });
-    inputPass.change(function() {
-        let defaultPass = inputUser.val();
-        chrome.storage.sync.set({ defaultPass });
     });
     $('input[type=radio][name=default_radio]').change(function() {
         chrome.storage.sync.set({ defaultType: this.value });
--- unipdFixer.js
+++ unipdFixer.js
@@ -1,17 +1,10 @@
 let inputUser = document.getElementById("j_username_js");
-let inputPass = document.getElementById("password");
 let inputRadioProf = document.getElementById("radio1");
 let inputRadioStudenti = document.getElementById("radio2");
 
 chrome.storage.sync.get("defaultUser", ({ defaultUser }) => {
     setTimeout(() => {
         inputUser.value = defaultUser;
-    }, 200);
-});
-
-chrome.storage.sync.get("defaultPass", ({ defaultPass }) => {
-    setTimeout(() => {
-        inputPass.value = defaultPass;
     }, 200);
 });
 
